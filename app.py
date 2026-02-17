d25251392404c9fe8ef1299933390f4460f39b40
Venkata Mohan Sai <mohansaichintakrindi@gmail.com>
2026-02-18 01:01:05 +0530

    Viewer mobile fix

--- app.py
+++ app.py
@@ -71,5 +71,4 @@ def files(name):
 
 # -------- RENDER DEPLOYMENT FIX --------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
